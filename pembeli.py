5b646ec20430361e3ce67ceda255a37f75419aa9
Atharr28 <atharkhairy@gmail.com>
2024-12-12 18:42:56 +0700

    Fitur Beli

--- pembeli.py
+++ pembeli.py
@@ -9,3 +9,23 @@ def mode_pembeli():
             break
         else:
             print("Sepertinya yang anda pilih di luar menu :) harap masukan yang sudah disediakan menu")
+
+
+def membeli_barang():
+    while True: 
+        user_milih = input("\nMasukkan nama produk yang ingin dibeli atau keluar dengan mengetik (keluar) : ").lower()
+        if user_milih == "keluar":
+            break
+        if user_milih in produk:
+            jumlah = int(input(f"Berapa Banyak Pilihan yang ingin dibeli : "))
+            if jumlah <= produk[user_milih]["stok"]:
+                produk[user_milih]["stok"] -= jumlah
+                total += jumlah * produk[user_milih]["harga"]
+                print(f"{jumlah} {user_milih} Di tambahkan ke kerangjang. Total : Rp.{total}")
+            else:
+                print("Maaf,stok tidak mencukupi")
+        else:
+            print("Mohon maaf produk tidak tersedia saat ini")
+
+    print(f"Total belanja anda {total}")
+    print("Terima kasih sudah berbelanja 😀")
